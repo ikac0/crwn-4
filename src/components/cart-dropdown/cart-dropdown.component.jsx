3a667562c4b43ad4113651562e7b3615b65909b7
Ilija <krsteski.ilija@gmail.com>
2020-12-30 23:32:59 +0100

    started creating the checkoutpage, he cart.dropdown message if empty

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -13,7 +13,10 @@ const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {
+        cartItems.length ?
         cartItems.map(x => <CartItem key={x.id} item={x} /> )
+        :
+        <span className="empty-message">Your cart is empty!</span>
       }
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
